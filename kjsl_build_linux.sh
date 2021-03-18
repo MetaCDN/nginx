418313ba5854e965e57142fb3e7a60faacf2c89c
Ubuntu <ubuntu@live-ss-na-2.c.metacdn-google.metacdn.com.internal>
2021-03-18 11:02:16 +0000

    KJSL TLS 1.3

--- kjsl_build_linux.sh
+++ kjsl_build_linux.sh
@@ -2,12 +2,12 @@
 git clone https://github.com/kevleyski/nginx-rtmp-module nginx-rtmp-module
 git clone https://github.com/kevleyski/ngx_devel_kit ngx_devel_kit 
 
-if [ ! -d openssl-1.0.2o ]; then
-wget http://www.openssl.org/source/openssl-1.0.2o.tar.gz
-tar -zxf openssl-1.0.2o.tar.gz
-cd openssl-1.0.2o
+if [ ! -d openssl-MetaCDN_20210318 ]; then
+wget https://github.com/MetaCDN/openssl/archive/MetaCDN_20210318.tar.gz
+tar -zxf MetaCDN_20210318.tar.gz
+cd openssl-MetaCDN_20210318
 ./Configure --prefix=/usr
-make
+make -j $(nproc)
 sudo make install
 cd ..
 fi
@@ -17,7 +17,7 @@ wget http://zlib.net/zlib-1.2.11.tar.gz
 tar -zxf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure
-make
+make -j $(nproc)
 sudo make install
 cd ..
 fi
@@ -27,7 +27,7 @@ wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
 tar -zxf pcre-8.42.tar.gz
 cd pcre-8.42
 ./configure
-make
+make -j $(nproc)
 sudo make install
 cd ..
 fi
@@ -37,12 +37,44 @@ mkdir -p /usr/nginx/nginx
 ./auto/configure --prefix=/etc/nginx \
 	--with-cc-opt="-I/usr/include" \
 	--with-ld-opt="-L/usr/lib" \
+ --with-select_module \
+            --with-poll_module \
+            --with-threads \
+            --with-file-aio \
+            --with-http_v2_module \
+            --with-http_realip_module \
+            --with-http_addition_module \
+            --with-http_xslt_module=dynamic \
+            --with-http_image_filter_module=dynamic \
+            --with-http_geoip_module=dynamic \
+            --with-http_sub_module \
+            --with-http_dav_module \
+            --with-http_flv_module \
+            --with-http_mp4_module \
+            --with-http_gunzip_module \
+            --with-http_gzip_static_module \
+            --with-http_auth_request_module \
+            --with-http_random_index_module \
+            --with-http_secure_link_module \
+            --with-http_degradation_module \
+            --with-http_slice_module \
+            --with-http_perl_module=dynamic \
+            --with-perl_modules_path=/usr/share/perl/5.26.1 \
+            --with-perl=/usr/bin/perl \
+            --http-log-path=/var/log/nginx/access.log \
+            --http-client-body-temp-path=/var/cache/nginx/client_temp \
+            --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+            --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+            --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+            --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+            --with-stream_realip_module \
+            --with-stream_geoip_module=dynamic \
         --with-compat \
- 	--add-module=../nginx_mod_smooth_streaming-1.9.6 \
         --with-openssl=/usr/include/openssl \
+	--with-openssl-opt=enable-tls1_3 \
         --with-http_stub_status_module
 
-make -j9
+make -j $(nproc)
 sudo make install
 
 nginx -V
